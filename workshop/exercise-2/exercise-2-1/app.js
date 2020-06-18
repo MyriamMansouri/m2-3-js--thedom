88ffe67d7b3ffe4c9ae518b90a37c3c09b17e2a6
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-17 23:47:30 -0400

    Final adjustments + some styling

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,6 +1,7 @@
 
 const FROGS = 3;
 const racers = [];
+const ranking = [];
 
 // Initialize DOM so frogs ready for the race
 for (let i = 0; i < FROGS ; i++) {
@@ -21,10 +22,14 @@ for (let i = 0; i < FROGS ; i++) {
 
     // add frog to the DOM
     let span = document.createElement("span");
-    span.className = "frog"
+    span.className = "frog length length-value"
     span.innerHTML = `${racingFrog.name} ${racingFrog.number}`;
     span.setAttribute("progress", racingFrog.progress);
     document.querySelector(`#${laneId}`).appendChild(span);
+
+    let div = document.createElement("div");
+    div.innerHTML = `<img src = "../assets/frogx50.png"  alt = "A frog"/>`;
+    document.querySelector(`#${laneId} .frog`).appendChild(div);
 
     // style frog <span> element
     span.style.backgroundColor = `${racingFrog.color}`
@@ -40,14 +45,40 @@ const racingFrog = function(frog) {
 
         if (frog.progress > 100) {
             frog.progress = 100;
-            console.log("End of the race");
+            ranking.push(frog);
+            console.log("End of the race for one of the frogs!");
             clearInterval(jumps);
         }
-        console.log(frog.progress)
         document.querySelector(`.track #lane-${frog.lane} .frog`).style.left = frog.progress + '%' ;
     }, delay);
 
 }
 
 // the race !
-racers.forEach( racer => racingFrog(racer) );
+racers.forEach( racer => {
+        racingFrog(racer);
+});
+
+// "And the winners are" message
+let rankingMessage = setInterval(function() {
+    if (ranking.length === FROGS) {
+        // Add ranking section to DOM
+        let h2 = document.createElement("h2");
+        h2.innerHTML = "The Podium 🏆";
+        document.querySelector("main").appendChild(h2);
+
+        let ol = document.createElement("ol");
+        ol.className = "podium";
+        document.querySelector("main").appendChild(ol);
+        ol.style.fontSize = "20px";
+
+        ranking.forEach( el => {
+            let li = document.createElement("li");
+            li.innerHTML = `#${el.number} ${el.name}\n`;
+            document.querySelector(".podium").appendChild(li);
+        }) 
+
+        clearInterval(rankingMessage);
+    }
+}, 1000);
+
